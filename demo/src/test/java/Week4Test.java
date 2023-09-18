8e5fb697920c7cb776d12e21b1a665eecfcb0a5f
lengocquanggit255 <126934622+lengocquanggit255@users.noreply.github.com>
2023-09-18 14:45:22 +0700

    junit oke

---
+++ demo/src/test/java/Week4Test.java
@@ -0,0 +1,47 @@
+
+public class Week4Test {
+    
+    public void testMax2Int1(){}
+
+    
+    public void testMax2Int2(){}
+
+    
+    public void testMax2Int3(){}
+
+    
+    public void testMax2Int4(){}
+
+    
+    public void testMax2Int5(){}
+
+    
+    public void testMinArray1(){}
+
+    
+    public void testMinArray2(){}
+
+    
+    public void testMinArray3(){}
+
+    
+    public void testMinArray4(){}
+
+    
+    public void testMinArray5(){}
+
+    
+    public void testCalculateBMI1(){}
+
+    
+    public void testCalculateBMI2(){}
+
+    
+    public void testCalculateBMI3(){}
+
+    
+    public void testCalculateBMI4(){}
+
+    
+    public void testCalculateBMI5(){}
+} 
