0b6dd2a13083bd0e86edc378625509abdc32b0af
Nguyễn Tuấn Hưng <nguyentuanhung4002@gmail.com>
2023-11-19 02:55:59 +0700

    update2

--- dictionary/src/main/java/org/openjfx/dictionary/SetColorBorder.java
+++ dictionary/src/main/java/org/openjfx/dictionary/SetColorBorder.java
@@ -8,21 +8,21 @@ import javafx.util.Duration;
 public class SetColorBorder {
 
     public void setBorderWrongButton(Button button) {
-        button.setStyle("-fx-border-width: 2px; -fx-background-color:  #9078BB; -fx-background-radius: 10px; -fx-border-color: red; -fx-text-fill: #FFFFFF; -fx-font-size: 20px;");
+        button.setStyle("-fx-border-width: 2px; -fx-border-radius: 25px; -fx-background-color:  #03A9F4; -fx-background-radius: 25px; -fx-border-color: red; -fx-text-fill: #FFFFFF; -fx-font-size: 20px;");
     }
 
     public void setBorderCorrectButton(Button button) {
-        button.setStyle("-fx-border-width: 2px; -fx-background-color:  #9078BB; -fx-background-radius: 10px; -fx-border-color: green; -fx-text-fill: #FFFFFF; -fx-font-size: 20px;");
+        button.setStyle("-fx-border-width: 2px; -fx-border-radius: 25px; -fx-background-color:  #03A9F4; -fx-background-radius: 25px; -fx-border-color: green; -fx-text-fill: #FFFFFF; -fx-font-size: 20px;");
     }
 
     public void setColorButton(Button button) {
-        button.setStyle("-fx-background-color:  #C9BCE4; -fx-background-radius: 10px; -fx-text-fill: #FFFFFF; -fx-font-size: 20px;");
+        button.setStyle("-fx-background-color:  #1144A6; -fx-background-radius: 25px; -fx-text-fill: #FFFFFF; -fx-font-size: 20px;");
     }
 
     public void resetBorderButton(Button button1, double delayInSeconds) {
         Timeline timeline = new Timeline(
             new KeyFrame(Duration.seconds(delayInSeconds), event -> {
-            button1.setStyle("-fx-border-width: 0px;  -fx-background-color:  #9078BB; -fx-background-radius: 10px; -fx-text-fill: #FFFFFF; -fx-font-size: 20px;");
+            button1.setStyle("-fx-border-width: 0px;  -fx-background-color:  #03A9F4; -fx-background-radius: 25px; -fx-text-fill: #FFFFFF; -fx-font-size: 20px;");
             })
         );
 
@@ -30,6 +30,6 @@ public class SetColorBorder {
     }
 
     public void resetColorButton(Button button) {
-        button.setStyle("-fx-background-color:  #9078BB; -fx-background-radius: 10px; -fx-text-fill: #FFFFFF; -fx-font-size: 20px;");
+        button.setStyle("-fx-background-color:  #03A9F4; -fx-background-radius: 25px; -fx-text-fill: #FFFFFF; -fx-font-size: 20px;");
     }
 }
