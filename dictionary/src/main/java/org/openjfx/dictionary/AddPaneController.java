64a8b4cf42c01e9040893755455e340d3b730140
lengocquanggit255 <126934622+lengocquanggit255@users.noreply.github.com>
2023-11-20 15:53:22 +0700

    upadate addPane

--- dictionary/src/main/java/org/openjfx/dictionary/AddPaneController.java
+++ dictionary/src/main/java/org/openjfx/dictionary/AddPaneController.java
@@ -7,13 +7,14 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
+import javafx.scene.web.HTMLEditor;
 
 public class AddPaneController {
     @FXML
     TextField newWordTextField;
 
     @FXML
-    TextField explainTextField;
+    HTMLEditor explainHTMLEditor;
 
     @FXML
     private Label messageLabel;
@@ -21,33 +22,45 @@ public class AddPaneController {
     @FXML
     public void saveNewWord(ActionEvent event) {
         String word_target = newWordTextField.getText();
-        String word_explain = explainTextField.getText();
+        String word_explain = explainHTMLEditor.getHtmlText();
 
         if (word_target == null || word_target.isEmpty()) {
-            messageLabel.setText("Please fill everything!");
+            showMessage("Please fill everything!", false);
             return;
         }
 
-        if (word_explain == null || word_explain.isEmpty()) {
-            messageLabel.setText("Please fill everything!");
+        if (explainHTMLEditor.getHtmlText().equals(
+                "<html dir=\"ltr\"><head></head><body contenteditable=\"true\"></body></html>")) {
+            showMessage("Please fill everything!", false);
             return;
         }
 
         if (!Helper.dictionary.contain(word_target)) {
             Helper.dictionary.put(new Word(word_target, word_explain));
-            messageLabel.setText("Successfully added!");
+            showMessage("Successfully added!", true);
             newWordTextField.setText("");
-            explainTextField.setText("");
+            explainHTMLEditor.setHtmlText("");
         } else {
-            messageLabel.setText("Words already existed!");
+            showMessage("Word already exists!", false);
             return;
+        }
+    }
+
+    private void showMessage(String message, boolean isSuccess) {
+        messageLabel.setText(message);
+        messageLabel.setVisible(true);
+        if (isSuccess) {
+            messageLabel.setStyle("-fx-background-color: green;");
+        } else {
+            messageLabel.setStyle("-fx-background-color: #e22134;");
         }
     }
 
     public void reload() {
         newWordTextField.setText("");
-        explainTextField.setText("");
+        explainHTMLEditor.setHtmlText("");
         messageLabel.setText("");
+        messageLabel.setVisible(false);
     }
 
 }
